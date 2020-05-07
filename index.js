b526fd8b8a3498824a6f72744a9f1ed94c13bae8
PierreLx <leroux.pierre.76@gmail.com>
2020-05-07 07:54:37 +0200

    cors 2

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true })) 
 // parse application/json
 app.use(bodyParser.json())
-app.use(cors())
+// app.use(cors())
 // app.options('*', cors())
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
 
 app.use('/public', express.static(__dirname + '/public'));
 
