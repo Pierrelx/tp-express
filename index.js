c83dccc276ff6a517d47711a781418c9a4bee7a6
PierreLx <leroux.pierre.76@gmail.com>
2020-05-07 07:57:57 +0200

    2

--- index.js
+++ index.js
@@ -11,21 +11,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 // app.use(cors())
 // app.options('*', cors())
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
 });
 
 app.use('/public', express.static(__dirname + '/public'));
